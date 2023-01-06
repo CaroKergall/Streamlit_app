2cf8cfac3adcfdc03874313b965fea68d0f0c22b
KERGALL Caroline <116085438+CaroKergall@users.noreply.github.com>
2023-01-06 14:40:56 +0100

    Add files via upload

--- Streamlit_build_data.py
+++ Streamlit_build_data.py
@@ -4,44 +4,67 @@ import seaborn as sns
 import plotly.express as px
 
 # ecrire du texte
-st.title('Hello Wilders, welcome to my application!')
-st.write("I enjoy to discover stremalit possibilities")
+st.title('Salut les Wilders, bienvenue sur mon app streamlit !')
+st.write("Voici le résultat de la quête d'odyssée")
 
-name = st.text_input("Please give me your name :")
-name_length = len(name)
-st.write("Your name has ",name_length,"characters")
+# name = st.text_input("Please give me your name :")
+# name_length = len(name)
+# st.write("Your name has ",name_length,"characters")
 
-# afficher un dataframe
-link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/weather2019.csv"
-df_weather = pd.read_csv(link)
-st.write(df_weather)
+# des boutons doivent être présents pour pouvoir filtrer les résultats par région (US / Europe / Japon).
+filter = st.multiselect(
+    'Choisissez un ou plusieurs continent à filtrer :',
+    [' US.', ' Europe.', ' Japan.'])
 
-# Here we use "magic commands":
-df_weather
+# st.write('You selected:', filter)
 
-# viz avec streamlit
-st.line_chart(df_weather['MAX_TEMPERATURE_C'])
+# dataframe
+link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv"
+df_cars = pd.read_csv(link)
 
-# viz avec seaborn
-viz_correlation = sns.heatmap(df_weather.corr(), 
-								center=0,
-								cmap = sns.color_palette("vlag", as_cmap=True)
-								)
+# mask = df_cars['continent'].isin(filter)
+# final = pd.DataFrame()
+# if filter : 
+#     for continent in filter : 
+#         filtered_df = df_cars[df_cars['continent'] == continent]
+#         final = final.append(filtered_df)
+#         st.write(final)
 
-st.pyplot(viz_correlation.figure)
+if filter:
+    filtered_df = df_cars.loc[df_cars['continent'].isin(filter)]
+else:
+    filtered_df = df_cars
 
-# viz avec plotly
-df = px.data.gapminder()
+st.write(filtered_df)
 
-fig = px.scatter(
-    df.query("year==2007"),
-    x="gdpPercap",
-    y="lifeExp",
-    size="pop",
-    color="continent",
-    hover_name="country",
-    log_x=True,
-    size_max=60,
-)
+# une analyse de corrélation et de distribution grâce à différents graphiques et des commentaires.
+if filter :
+    viz_correlation = sns.heatmap(filtered_df.corr(), 
+                                    center=0,
+                                    cmap = sns.color_palette("vlag", as_cmap=True)
+                                    )
+    st.write('Analyse de correlation sur les voitures')
+    st.pyplot(viz_correlation.figure)
+else :
+    viz_correlation = sns.heatmap(df_cars.corr(), 
+                                    center=0,
+                                    cmap = sns.color_palette("vlag", as_cmap=True)
+                                    )
+    st.write('Analyse de correlation sur les voitures')
+    st.pyplot(viz_correlation.figure)
 
-st.plotly_chart(fig, theme=None, use_container_width=True)
+if filter :
+    grouped = filtered_df.groupby('cylinders')
+    mean_cylinders = grouped['weightlbs'].mean()
+    mean_cylinders.plot.bar()
+
+    st.write('Moyenne des cylindrées par poids du véhicule')
+    st.bar_chart(mean_cylinders)
+else :
+    grouped = df_cars.groupby('cylinders')
+    mean_cylinders = grouped['weightlbs'].mean()
+    mean_cylinders.plot.bar()
+
+    st.write('Moyenne des cylindrées par poids du véhicule')
+    st.bar_chart(mean_cylinders)
+
